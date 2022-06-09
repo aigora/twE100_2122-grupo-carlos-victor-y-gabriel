b605582c8723da4c9cccbf9275106a5e208543e7
Gabriel-S-J <79969664+Gabriel-S-J@users.noreply.github.com>
2022-06-09 18:45:50 +0200

    Create lib.h

---
+++ lib.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
+#include <windows.h>
+#include <time.h>
+#define H 45
+#define V 13
+    int juegomillonario();
+    int juegocompletoporteria();
+    int juegooperaciones();
+    int ahorcado();
+    int juegomemory();
+    int juegoaleatorios();
+    void espacio();
